711148fc499e941833fe1405bcd504f917e02488
Chanuka Gamage <shashiduchanuka@gmail.com>
2026-03-10 23:18:52 +0530

    Add files via upload

---
+++ Overloading.java
@@ -0,0 +1,22 @@
+class calculater{
+	int add(int a,int b){
+		return a+b;
+	}
+	
+	int add(int a,int b,int c){
+		return a+b+c;
+		
+	}
+}
+
+public class Overloading {
+	public static void main (String[] args){
+		calculater obj=new calculater();
+		System.out.println(obj.add(12,12);
+		System.out.println(obj.add(12,12,12);
+	}
+}
+/* ---------Overloading------------
+1.same method name with different parameters
+2.parameters must be different
+*/
